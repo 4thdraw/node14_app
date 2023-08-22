c1d45d2907b8630e188e55429ed8fe4a56736a79
4thdraw <4thdraw2@gmail.com>
2023-08-22 11:46:41 +0900

    리액트가 요청할 라우터 설정완료

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const app = require('express')();
+const navi = require("./api/navi");
+const banner = require("./api/banner");
+const interview = require("./api/interview");
+
+// 리액트가 요청할 수 있도록 주소생성하기
+app.use('/', interview);
+app.use('/b', banner);
+app.use('/navi', navi);
+
+
+app.use((req, res) =>{
+    res.status(404).sendFile( __dirname+'/public/nopage.html')
+})
 
 app.listen(8000, ()=>{
     console.log("서버정상구동중")
